c06ee6bca49056efcc6695cf9b4618f39e89212a
astel <kiyomillefeuille@gmail.com>
2026-06-01 19:51:55 +0900

    chore: update vozltop to v0.3.0 with correct sha256 hashes

--- Formula/vozltop.rb
+++ Formula/vozltop.rb
@@ -7,19 +7,19 @@ class Vozltop < Formula
   on_macos do
     on_arm do
       url "https://github.com/astail/vozltop/releases/download/v0.3.0/vozltop-0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "f14d21aabda64fee1d4eb6ebbdb58743804d8e760b5e51c73991d5b82f935ae5"
+      sha256 "77b625431b836359c2550a162cd0cbf5d9b6c21866495336c55a0ca43597f4bd"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/astail/vozltop/releases/download/v0.3.0/vozltop-0.3.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "85ae62fc5f710c8dd0accae0cd33a575a89e436c22e41c39696cb8dac76e1006"
+      sha256 "7ef4f900a72b3f12412461f45bc6982e7b78c930936f649ae1ca80e010b48870"
     end
 
     on_arm do
       url "https://github.com/astail/vozltop/releases/download/v0.3.0/vozltop-0.3.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "9e2076eae82b163fec6524684e68c1528b5473d05c02a2fda8ebbe3be9d8bf57"
+      sha256 "720ae8950aae075f31e5a36d2484bba32898e7e8a93989b44603e594403b2685"
     end
   end
 
